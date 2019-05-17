37455dc742a4261f75e513af2fe011ae580dc052
gkdnr5148 <50752666+gkdnr5148@users.noreply.github.com>
2019-05-18 04:20:46 +0900

    Add files via upload

---
+++ scrim.py
@@ -0,0 +1,24 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("KSP_SCRIM BOT")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!서버"):
+        await message.channel.send("```도쿄GCE１```")
+    if message.content.startswith("!안내"):
+        await message.channel.send("@everyone 금일부터 시간표가 변경되었습니다.")
+    if message.content.startswith("!카운트1"):
+        await message.channel.send("a!scrim countdown  1 곧시작합니다.")
+
+client.run("NTc4OTA1NzA5MDM5NzE0MzI3.XN7mHw.1ZpiDQnUNELxXQ18di40-fjmxiU")
